4189a2e41c9ef9f1e10d2ff64d6d15b10e080022
Ayala <ayalabad@post.bgu.ac.il>
2021-12-29 00:59:39 +0200

    using queue

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from ctypes import WINFUNCTYPE, FormatError
 import socket
 import time
 import threading
+import queue
 import struct
 import math
 from typing import List, Match
@@ -22,7 +23,7 @@ message_type = bytes([0x2])
 server_port = str(WELCOME_PORT).encode(FORMAT)
 udp_msg = UDP_msg(magic_cookie, message_type, server_port)
 msg = udp_msg.get_msg()
-sock_list = []
+answers_queue : queue.Queue = []
 player_names = [None, None]
 winner_name = None
 need_to_run = True
@@ -102,27 +103,39 @@ def client_registration_running(registration_locker : threading.Lock, playing_lo
         client_name = client_msg_bytes.decode(FORMAT)
         print(client_name)
         player_names[index_name] = client_name
-        sock_list.insert(index_name, server_tcp_client_sock)
+        # sock_list.insert(index_name, server_tcp_client_sock)
         registration_locker.release()
         playing_locker.acquire()
         starting_msg = welcome_msg.format(player_names[0], player_names[1], "how much is 2+2")
         starting_msg_in_bytes = starting_msg.encode(FORMAT)
         server_tcp_client_sock.sendall(starting_msg_in_bytes)
-        server_tcp_client_sock.settimeout(10)
+        # server_tcp_client_sock.settimeout(10)
+        answer_thread = threading.Thread(target = recieve_answer_running, args = (server_tcp_client_sock,))
+        answer_thread.start()
         try:
-            client_answer_in_bytes = server_tcp_client_sock.recv(TCP_MSG_SIZE)
-            answering_locker.acquire()
+            client_answer_in_bytes = answers_queue.get(True, 10)
             client_answer = client_answer_in_bytes.decode(FORMAT)
             if (winner_name == None):
                 if(client_answer == "4"):
                     winner_name = client_name
                 else:
                     winner_name = player_names[abs(index_name - 1)]
-            answering_locker.release()
-        except:
-            if(winner_name == None):
-                winner_name = "tie"
-        server_tcp_client_sock.settimeout(None)
+        except TimeoutError:
+            winner_name = "tie"
+        # try:
+        #     client_answer_in_bytes = server_tcp_client_sock.recv(TCP_MSG_SIZE)
+        #     answering_locker.acquire()
+        #     client_answer = client_answer_in_bytes.decode(FORMAT)
+            # if (winner_name == None):
+            #     if(client_answer == "4"):
+            #         winner_name = client_name
+            #     else:
+            #         winner_name = player_names[abs(index_name - 1)]
+        #     answering_locker.release()
+        # except:
+        #     if(winner_name == None):
+        #         winner_name = "tie"
+        # server_tcp_client_sock.settimeout(None)
         # if (winner_name == None):
         #     if(client_answer == "4"):
         #         winner_name = client_name
@@ -134,8 +147,14 @@ def client_registration_running(registration_locker : threading.Lock, playing_lo
         server_tcp_client_sock.sendall(ending_msg_in_bytes)
         # server_tcp_client_sock.close()
         playing_locker.release()
-        sock_list.pop
+        # sock_list.pop
 
+def recieve_answer_running(server_tcp_client_sock : socket.socket):
+    global answers_queue
+    client_answer_in_bytes = server_tcp_client_sock.recv(TCP_MSG_SIZE)
+    answering_locker.acquire()
+    client_answer = client_answer_in_bytes.decode(FORMAT)
+    answers_queue.put (client_answer)
 
 def start():
     print("Server started, listening on IP address {}".format(IP))
